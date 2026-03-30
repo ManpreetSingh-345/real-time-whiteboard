f6388c687c82ac53c5781bdaa852a3a1169ac6de
Manpreet Singh <msingh87@uoguelph.ca>
2026-03-30 19:59:37 -0400

    Add dependencies and allow communications with frontend

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 const app = express();
+
 dotenv.config();
+app.use(cors()); // Allows all origins by default
 
 app.get("/", (req, res) => {
+  console.log("Request received");
   res.send("Hello, World!");
 });
 
