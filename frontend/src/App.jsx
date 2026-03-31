f123bb3e292808f0647f027eb14628ca7ed8f635
Manpreet Singh <msingh87@uoguelph.ca>
2026-03-30 20:49:58 -0400

    Add: Tailwind CSS to project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import axios from "axios";
 
 function App() {
-  return <div>Hello</div>;
+  return <div className="font-poiret text-5xl">White Board Application</div>;
 }
 
 export default App;
