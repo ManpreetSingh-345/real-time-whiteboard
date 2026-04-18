2a1c983d11ddd8a6565080d6a27e59e134d2af4f
Manpreet Singh <msingh87@uoguelph.ca>
2026-04-18 13:15:06 -0400

    Add header and styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,23 @@ import axios from "axios";
 
 function App() {
   return (
-    <div className="font-poiret">
-      <main className="h-screen flex flex-col gap-3 justify-center items-center bg-linear-to-r from-70% from-white to-[#D6B588]">
+    <div className="font-sora">
+      <header className="flex justify-between p-7 fixed top-0 right-0 left-0">
+        <div className="text-3xl">WhiteBoard</div>
+        <div className="flex gap-3">
+          <button className="hover:cursor-pointer underline underline-offset-4">
+            Features
+          </button>
+          <button className="hover:cursor-pointer">Pricing</button>
+          <button className="hover:cursor-pointer">Blog</button>
+          <button className="hover:cursor-pointer">Resources</button>
+        </div>
+        <div className="flex gap-3">
+          <button>Log in</button>
+          <button>Sign up</button>
+        </div>
+      </header>
+      <main className="h-screen flex flex-col gap-3 justify-center items-center bg-radial-[at_50%_75%] from-70% from-[#D7DEDC] to-[#9A879D]">
         <div className="text-4xl">Welcome to the White Board</div>
         <p className="text-2xl">Where ideas meet reality</p>
         <button className="border rounded-2xl px-5 py-2 hover:cursor-pointer hover:bg-black hover:text-white duration-300">
